da95180e63381afa5a0aa64f7b67c1c66f8f178d
sanyou <sanyou1997@163.com>
2020-06-11 15:03:17 -0500

    correct model path

--- jyforestry_server/settings.py
+++ jyforestry_server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z^_*9ss9*14c+k45=x1-%jir5tpzw*)%u+equzf76v3&uf1ll1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
