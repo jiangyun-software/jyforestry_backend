db545d7e197188c943d659b870cc105bb8d81f12
sanyou <sanyou1997@163.com>
2020-06-12 02:49:26 -0500

    receive form data

--- basic_model/views.py
+++ basic_model/views.py
@@ -5,18 +5,28 @@ from .predict_model import predict_cover_type
 from sklearn import preprocessing
 from django.views.decorators.csrf import csrf_exempt
 
+attributes = ['Elevation', 'Aspect', 'Slope',
+'Horizontal_Distance_To_Hydrology', 'Vertical_Distance_To_Hydrology',
+'Horizontal_Distance_To_Roadways', 'Hillshade_9am', 'Hillshade_Noon',
+'Hillshade_3pm', 'Horizontal_Distance_To_Fire_Points',
+'Wilderness_Area1', 'Wilderness_Area2', 'Wilderness_Area3',
+'Wilderness_Area4', 'Soil_Type1', 'Soil_Type2', 'Soil_Type3',
+'Soil_Type4', 'Soil_Type5', 'Soil_Type6', 'Soil_Type7', 'Soil_Type8',
+'Soil_Type9', 'Soil_Type10', 'Soil_Type11', 'Soil_Type12',
+'Soil_Type13', 'Soil_Type14', 'Soil_Type15', 'Soil_Type16',
+'Soil_Type17', 'Soil_Type18', 'Soil_Type19', 'Soil_Type20',
+'Soil_Type21', 'Soil_Type22', 'Soil_Type23', 'Soil_Type24',
+'Soil_Type25', 'Soil_Type26', 'Soil_Type27', 'Soil_Type28',
+'Soil_Type29', 'Soil_Type30', 'Soil_Type31', 'Soil_Type32',
+'Soil_Type33', 'Soil_Type34', 'Soil_Type35', 'Soil_Type36',
+'Soil_Type37', 'Soil_Type38', 'Soil_Type39', 'Soil_Type40',]
+
 @csrf_exempt
 def test(request):
     if request.method == "POST":
-        elevation = float(request.POST['elevation'])
-        aspect = float(request.POST['aspect'])
-        data = [[elevation,  aspect , -0.89,  1.65,  0.47,  1.47, -0.09,  1.32,
-            0.87,  0.93,  1.79, -0.18, -0.85, -0.67, -0.16, -0.21, -0.26,
-        -0.24, -0.11, -0.21,  0.  , -0.01, -0.03, -0.41, -0.17, -0.12,
-        -0.18, -0.11,  0.  , -0.09, -0.21, -0.06, -0.06, -0.1 , -0.03,
-        -0.15,  4.36, -0.13, -0.01, -0.06, -0.03, -0.02, -0.31, -0.22,
-        -0.15, -0.22, -0.21, -0.04, -0.08, -0.03, -0.05, -0.22, -0.21,
-        -0.18]]
+        data = [[]]
+        for attribute in attributes:
+            data[0].append(float(request.POST[attribute]))
         data = preprocessing.StandardScaler().fit(data).transform(data)
         res = predict_cover_type(data)
         return HttpResponse(str(res))
